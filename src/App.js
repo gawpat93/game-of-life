995b0917c660398e1ff429373744d658f78afe7c
Patryk Gawryszewski <patgaw09318@gmail.com>
2020-07-28 14:02:53 +0200

    Initial condition generation, canvas

--- src/App.js
+++ src/App.js
@@ -4,10 +4,22 @@ import "./App.css";
 import Canvas from "./components/Canvas";
 
 function App() {
+  const max = 20;
+
+  let table = [];
+  for (var y = 0; y < max; y++) {
+    let tmpRow = [];
+    for (var x = 0; x < max; x++) {
+      let random = Math.random();
+      tmpRow.push(Math.round(random));
+    }
+    table.push(tmpRow);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <Canvas />
+        <Canvas table={table} />
       </header>
     </div>
   );
--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Canvas.css";
 
-const Canvas = () => {
-  let list = [
-    [0, 0, 1],
-    [0, 1, 0],
-    [0, 0, 0],
-  ];
+const Canvas = (props) => {
+  const [time, setTime] = useState(Date.now());
+  useEffect(() => {
+    const interval = setInterval(() => setTime(Date.now()), 1000);
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
+  let table = props.table;
+  //Any live cell with two or three live neighbours survives.
+  //Any dead cell with three live neighbours becomes a live cell.
+  //All other live cells die in the next generation. Similarly, all other dead cells stay dead.
+
+  for (var y = 0; y < table.length; y++) {
+    for (var x = 0; x < table[y].length; x++) {}
+  }
   return (
-    <div className="canvas">
-      {list.map((y, y_index) =>
-        y.map((x, x_index) => (
-          <div row={y_index} column={x_index}>
-            {x}
-          </div>
-        ))
-      )}
+    <div className="table">
+      {table.map((y, y_index) => (
+        <div className="row">
+          {y.map((x, x_index) => (
+            <div
+              row={y_index}
+              column={x_index}
+              className={x === 1 ? "coll alive" : "coll"}
+            ></div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 };
