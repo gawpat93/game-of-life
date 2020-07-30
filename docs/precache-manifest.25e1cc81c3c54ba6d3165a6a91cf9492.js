b3b079f931403fd3dfc6602740e26a4aa8b6d808
Patryk Gawryszewski <patgaw09318@gmail.com>
2020-07-30 21:46:16 +0200

    Demo - for githubages

---
+++ docs/precache-manifest.25e1cc81c3c54ba6d3165a6a91cf9492.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abbbe87c912024bb65b10f8d3851804a",
+    "url": "/game-of-life/index.html"
+  },
+  {
+    "revision": "96b4bb087f62356950f7",
+    "url": "/game-of-life/static/css/main.d50ec90b.chunk.css"
+  },
+  {
+    "revision": "e7950cee3c021d55bf7e",
+    "url": "/game-of-life/static/js/2.bbe09184.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/game-of-life/static/js/2.bbe09184.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96b4bb087f62356950f7",
+    "url": "/game-of-life/static/js/main.dbf48cf4.chunk.js"
+  },
+  {
+    "revision": "a6893dc5f1659096e143",
+    "url": "/game-of-life/static/js/runtime-main.1dafd470.js"
+  }
+]);
